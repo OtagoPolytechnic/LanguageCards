c7d4b7ae40e321e8eb59cfbd39af875a425f6f22
tsgardiner <tsgardiner@windowslive.com>
2016-08-01 15:26:45 +1200

    Models Created

---
+++ app/LanguageApp/LanguageApp/LanguageApp/Database/Models/WordPair.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using SQLitePCL;
+using SQLiteNetExtensions.Attributes;
+
+namespace LanguageApp.Database.Models
+{
+    [Table("WordPair")]
+    public class WordPair
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+
+        [ForeignKey(typeof(WordRecord))]
+        public int original { get; set; }
+
+        [ForeignKey(typeof(WordRecord))]
+        public int translation { get; set; }
+    }
+}
---
+++ app/LanguageApp/LanguageApp/LanguageApp/Database/Models/WordRecord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using System.ComponentModel;
+
+namespace LanguageApp.Database.Models
+{
+    [Table("WordRecord")]
+    public class WordRecord
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string word { get; set; }
+        public string language { get; set; }
+        public string description { get; set; }
+        public DateTime dateUpdate { get; set; }
+        public DateTime dateCreated { get; set; }
+        public bool publish { get; set; }      
+
+    }
+}
