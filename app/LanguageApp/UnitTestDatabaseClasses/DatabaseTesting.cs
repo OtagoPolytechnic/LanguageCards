b7c099bcf5fa39c4b17067d89904602f2b448ef1
tsgardiner <tsgardiner@windowslive.com>
2016-08-02 13:56:45 +1200

    JSON date converter test written

--- app/LanguageApp/UnitTestDatabaseClasses/DatabaseTesting.cs
+++ app/LanguageApp/UnitTestDatabaseClasses/DatabaseTesting.cs
@@ -11,8 +11,15 @@ namespace UnitTestDatabaseClasses
         [TestMethod]
         public void JsonParserStringToDateTimeConverter()
         {
+            JsonParser jsonParser = new JsonParser();
+            
+            String jsonDate = "2013-01-16T08:16:59.844Z";
 
+            DateTime expected = new DateTime(2013, 1, 16, 8, 16, 59);
 
+            DateTime actual = jsonParser.JsonDateCoverter(jsonDate);
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
