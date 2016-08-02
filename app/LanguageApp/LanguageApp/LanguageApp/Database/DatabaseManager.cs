cde6d693096d33937438cd6c45b1a5659b708e40
tsgardiner <tsgardiner@windowslive.com>
2016-08-03 11:41:28 +1200

    Web Response Method

--- app/LanguageApp/LanguageApp/LanguageApp/Database/DatabaseManager.cs
+++ app/LanguageApp/LanguageApp/LanguageApp/Database/DatabaseManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,21 @@ namespace LanguageApp.Database
 {
     public class DatabaseManager
     {
-        //Handle connections and commuication between each class.               
+        //Handle connections and commuication between each class.   
+
+        public Uri uri { get; set; }
+        public HttpWebRequest webRequest;
+        public NetworkCredential credentials;
+        public HttpWebResponse webResponse;
+
+
+        public DatabaseManager()
+        {
+
+        }     
+
+
+       
 
     }
 }
---
+++ app/LanguageApp/LanguageApp/LanguageApp/Database/ExternalAsync.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageApp.Database
+{
+    public class ExternalAsync
+    {
+        //Request data from the backend via httprequest / api call.
+
+        public ExternalAsync()
+        {
+
+        }
+
+        public HttpWebResponse CallApi(String apiString, String accountKey)
+        {                                  
+            HttpWebResponse webResponse = null;  
+            try
+            {
+                Uri uri = new Uri(apiString);
+                HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(uri);
+
+                //Credentials currently not used.
+                //accountKey will be the incomming password / user name. If required by the api desgin.
+                NetworkCredential credentials = new NetworkCredential();
+                webRequest.Credentials = credentials;       
+
+                return webResponse; 
+            }
+            catch (WebException e)
+            {
+                Debug.WriteLine("Web Exception:" + e.ToString());
+                throw;   
+            }
+        }
+
+    }
+}
