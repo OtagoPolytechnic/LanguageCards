03bc6f5b7229d93dd5d9a85fa5839d7ab8e05763
iiSABRE <cameronthomasjackson@gmail.com>
2016-08-02 14:51:03 +1200

    Successful filtering of Words between two datetime()

--- admin/wordproject/views.py
+++ admin/wordproject/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.core import serializers
 from  django.http import HttpResponse
 from django.utils import timezone
-from django.utils import timezone
+import datetime
 from wordproject.models import WordRecord
 
 
@@ -13,8 +13,8 @@ def word_json(request):
     return HttpResponse(data, content_type='application/json')
 	
 def word_json_update(request):
-    first = datetime.date(2016,1,1)
-    last = timezone.now()
-    words = WordRecord.objects.filter(dateCreated__range=(first, last)
+    first_date = datetime.date(2016, 5, 11)
+    last_date = datetime.date(2016, 8, 20)
+    words = WordRecord.objects.filter(dateUpdated__range=(first_date, last_date))
     data = serializers.serialize("json", words)
     return HttpResponse(data, content_type='application/json')
