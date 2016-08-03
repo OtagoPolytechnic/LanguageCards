e5e9b4f820c1725fbb7ec9eabdb56feabb471ead
acraig94 <acraig94@gmail.com>
2016-08-03 14:57:28 +1200

    Created SQLite Interface & Droid/IOS SQLiteService Classses

---
+++ app/LanguageApp/LanguageApp/LanguageApp.Droid/Database/SQLiteService.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using LanguageApp.Droid.Database;
+using LanguageApp.Database;
+using Xamarin.Forms;
+using System.IO;
+
+[assembly: Dependency(typeof(SQLiteService))]
+namespace LanguageApp.Droid.Database
+{
+    public class SQLiteService
+    {
+
+        
+    }
+}
---
+++ app/LanguageApp/LanguageApp/LanguageApp.iOS/Database/SQLiteService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using LanguageApp.iOS.Database;
+using LanguageApp.Database;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLiteService))]
+namespace LanguageApp.iOS.Database
+{
+    public class SQLiteService : ISQLiteConnection
+    {
+    }
+}
