6c9e310c330641dbdccd157c5660f1e57e02d23c
Jared <jared.darvilljackson@gmail.com>
2016-08-25 10:27:57 +1200

    Added date fields to all tables

--- admin/wordproject/models.py
+++ admin/wordproject/models.py
@@ -18,6 +18,8 @@ class WordRecord(models.Model):
 class WordPair(models.Model):
     original = models.ForeignKey('WordRecord', on_delete=models.CASCADE, related_name='original_word')
     translation = models.ForeignKey('WordRecord', on_delete=models.CASCADE, related_name='translation_word')
+    dateCreated = models.DateTimeField(auto_now_add=True)
+    dateUpdated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.original.word
@@ -25,11 +27,15 @@ class WordPair(models.Model):
 
 class Sound(models.Model):
     blob = models.FileField()
+    dateCreated = models.DateTimeField(auto_now_add=True)
+    dateUpdated = models.DateTimeField(auto_now=True)
 
 
 class SoundPair(models.Model):
     wordpair = models.ForeignKey('WordPair', on_delete=models.CASCADE, )
     sound = models.ForeignKey('Sound', on_delete=models.CASCADE, )
+    dateCreated = models.DateTimeField(auto_now_add=True)
+    dateUpdated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.wordpair.original
