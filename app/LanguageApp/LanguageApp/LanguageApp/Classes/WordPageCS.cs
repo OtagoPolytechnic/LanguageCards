ef40282b10552c8b7c7c3f5fe0d6e52de34da40d
acraig94 <acraig94@gmail.com>
2016-09-02 18:41:07 +1200

    Added AppManager & images for UI

--- app/LanguageApp/LanguageApp/LanguageApp/Classes/WordPageCS.cs
+++ app/LanguageApp/LanguageApp/LanguageApp/Classes/WordPageCS.cs
@@ -11,33 +11,48 @@ namespace LanguageApp.Classes
     {
         public WordPageCS(DisplayObject displayObject)
         {
-            var padding = new Thickness(0, Device.OnPlatform(250, 250, 0), 0, 0);
+            Padding = new Thickness(5, Device.OnPlatform(250, 100, 0), 5, 100);
+
+            BackgroundColor = Color.Blue;
+
+            //BackgroundImage = "ira_mountain.jpg";
+            //this.Content { };
+
+            var mountainBanner = new Image { };
+            mountainBanner.Source = "ira_mountain.jpg";
 
 
 
-            Label wordLabel = new Label
+            var cardLayout = new StackLayout
             {
-                Text = displayObject.orginal,
-                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
-                HorizontalOptions = LayoutOptions.Center
+                Children = {
+                    new Frame {
+                        Content = mountainBanner,
+                    },
+                    new Label {
+                        Text = displayObject.orginal,
+                        FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                        HorizontalOptions = LayoutOptions.Center
+                        },
+                    new Label {
+                        Text = displayObject.translation,
+                        Font = Font.Default,
+                        FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                        HorizontalOptions = LayoutOptions.Center
+                        }
+                }
             };
 
-
-
-            Label wordLabel2 = new Label
-            {
-                Text = displayObject.translation,
-                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
-                HorizontalOptions = LayoutOptions.Center
+            this.Content = new Frame {
+                Content = cardLayout,
+                OutlineColor = Color.Lime,
+                HasShadow = true,
+                BackgroundColor = Color.White, Opacity = 1
+                //Margin = new Thickness()
+                
             };
 
-            var stackLayout = new StackLayout
-            {
-                Children = { wordLabel, wordLabel2 }
-
-            };
-            Padding = padding;
-            this.Content = stackLayout;
+            
         }
     }
 }
